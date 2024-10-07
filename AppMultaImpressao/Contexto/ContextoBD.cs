bb109fa5fc17f4603743409936f4ee3529a79efe
ReinaldoLimaXP <reinaldolimaxp@outlook.com>
2024-10-07 17:40:04 -0400

    Adicionar arquivos de projeto.

---
+++ AppMultaImpressao/Contexto/ContextoBD.cs
@@ -0,0 +1,17 @@
+﻿using AppMulta.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMulta.Contexto
+{
+    public class ContextoBD : DbContext
+    {
+        public ContextoBD(DbContextOptions<ContextoBD> options) : base(options)
+        {
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Multa> Multas { get; set; }
+    }
+}
+
---
+++ AppMultaImpressao/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppMulta.Models
+{
+    [Table("veiculo")]
+    public class Veiculo
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("modelo")]
+        public string? Modelo { get; set; }
+        [Column("marca")]
+        public string? Marca { get; set; }
+        [Column("placa")]
+        public string? Placa { get; set; }
+
+        public List<Multa>? Multas { get; set; }
+    }
+}
+
